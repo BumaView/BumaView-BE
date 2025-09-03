9502636abd44dd143862f212b85e17046199cdbf
심현진 <hyunjin.dev143@gmail.com>
2025-09-03 13:48:54 +0900

    Init::프로젝트 생성

---
+++ src/main/java/co/kr/bumaview/BumaviewApplication.java
@@ -0,0 +1,13 @@
+package co.kr.bumaview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumaviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumaviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/bumaview/BumaviewApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.bumaview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumaviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
