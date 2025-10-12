613f205a2dec3102438fa6f6dd36df102dd419c8
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-12 22:10:40 +0900

    feat: enhance JwtProvider with access and refresh token creation and validation methods

--- src/main/java/co/kr/bumaview/global/security/jwt/JwtProvider.java
+++ src/main/java/co/kr/bumaview/global/security/jwt/JwtProvider.java
@@ -1,18 +1,26 @@
 package co.kr.bumaview.global.security.jwt;
 
+import co.kr.bumaview.domain.auth.domain.RefreshToken;
 import co.kr.bumaview.domain.auth.domain.repository.TokenRepository;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
+import co.kr.bumaview.domain.auth.presentation.dto.req.TokenRefreshRequestDto;
+import co.kr.bumaview.domain.user.domain.CustomUserDetails;
+import co.kr.bumaview.domain.user.domain.type.Authority;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -46,4 +54,106 @@ public class JwtProvider {
             throw new IllegalArgumentException("유효하지 않은 토큰입니다.");
         }
     }
+
+    public String createAccessToken(Long userId, String userType) {
+        if("admin".equals(userType)) {
+            return Jwts.builder()
+                    .setSubject("AccessToken")
+                    .claim("userId", userId)
+                    .claim("userType", userType)
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY))
+                    .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+                    .compact();
+        } else {
+            return Jwts.builder()
+                    .setSubject("AccessToken")
+                    .claim("userId", userId)
+                    .claim("userType", userType)
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY))
+                    .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+                    .compact();
+        }
+    }
+    public String createRefreshToken(Long userId, Authority userType) {
+
+        String refreshToken = Jwts.builder()
+                .setSubject("RefreshToken")
+                .claim("userId", userId)
+                .claim("userType", userType)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_VALIDITY))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
+                .compact();
+
+        tokenRepository.findById(userId)
+                .ifPresentOrElse(
+                        token -> token.updateToken(refreshToken), // update
+                        () -> tokenRepository.save(new RefreshToken(userId, refreshToken, userType)) // insert
+                );
+        return refreshToken;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.JwtException | IllegalArgumentException e) {
+            log.info("[JwtProvider] 토큰 검증 실패: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        Long userId = claims.get("userId", Long.class);
+        String userType = claims.get("userType").toString();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        if ("admin".equalsIgnoreCase(userType)) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        } else if ("user".equalsIgnoreCase(userType)) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+
+        CustomUserDetails userDetails = new CustomUserDetails(userId, userType);
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+    }
+
+
+    public boolean isValidRefreshToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public String createAccessTokenByRefreshToken(TokenRefreshRequestDto refreshToken) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseClaimsJws(refreshToken.getRefreshToken())
+                .getBody();
+
+        Long userId = claims.get("userId", Long.class);
+        String userType = claims.get("userType", String.class);
+
+        return createAccessToken(userId, userType);
+    }
+
 }
