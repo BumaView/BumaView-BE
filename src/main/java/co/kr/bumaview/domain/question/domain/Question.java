230d1969a57a73e6c9fd93f7a5b2cc20844f9acf
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 06:04:49 +0900

    feat: add Question entity with fields for question details and relationships

---
+++ src/main/java/co/kr/bumaview/domain/question/domain/Question.java
@@ -0,0 +1,37 @@
+package co.kr.bumaview.domain.question.domain;
+
+import co.kr.bumaview.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="questions")
+@Getter
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+
+    @Column(columnDefinition = "json")
+    private String tad; //-> json
+
+    private String company;
+
+    private Long year;
+
+    private String category;
+
+    private String field;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User user;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
