c080e8032621dfa9631f75aa2ff99f5f5b0c962a
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 04:29:18 +0900

    refactor: rename findByUserId method to findById and update TokenRepository to use String as ID type

--- src/main/java/co/kr/bumaview/domain/auth/domain/repository/TokenRepository.java
+++ src/main/java/co/kr/bumaview/domain/auth/domain/repository/TokenRepository.java
@@ -2,15 +2,8 @@ package co.kr.bumaview.domain.auth.domain.repository;
 
 import co.kr.bumaview.domain.auth.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
-
-public interface TokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface TokenRepository extends JpaRepository<RefreshToken, String> {
 
     int deleteByRefreshToken(String token);
-
-    @Query("SELECT r FROM RefreshToken r WHERE r.id = :id")
-    Optional<RefreshToken> findByUserId(@Param("id") String id);
 }
