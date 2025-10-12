f6893328be9696139d70fd6f0e339bc09685b909
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 08:16:07 +0900

    refactor: 응답 구조 수정

--- src/main/java/co/kr/bumaview/domain/question/presentation/dto/res/QuestionResponse.java
+++ src/main/java/co/kr/bumaview/domain/question/presentation/dto/res/QuestionResponse.java
@@ -3,8 +3,18 @@ package co.kr.bumaview.domain.question.presentation.dto.res;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public class QuestionResponse {
     private String message;
+    private List<RegisteredQuestion> registeredQuestions;
+
+    @Getter
+    @AllArgsConstructor
+    public static class RegisteredQuestion {
+        private Long id;
+        private String q;
+    }
 }
