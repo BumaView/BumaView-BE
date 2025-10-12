5ea11e4d6f729fb1aa96549db5030ed321dc40b6
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 03:51:02 +0900

    refactor: change deleteByRefreshToken return type to int and add findByUserId method

--- src/main/java/co/kr/bumaview/domain/auth/domain/repository/TokenRepository.java
+++ src/main/java/co/kr/bumaview/domain/auth/domain/repository/TokenRepository.java
@@ -2,10 +2,14 @@ package co.kr.bumaview.domain.auth.domain.repository;
 
 import co.kr.bumaview.domain.auth.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<RefreshToken, Long> {
 
-    Optional<RefreshToken> deleteByRefreshToken(String token);
+    int deleteByRefreshToken(String token);
+
+    @Query("SELECT r FROM RefreshToken r WHERE r.id = :id")
+    Optional<RefreshToken> findByUserId(String id);
 }
