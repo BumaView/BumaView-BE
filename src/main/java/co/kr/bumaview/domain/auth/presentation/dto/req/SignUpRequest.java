7424c9ae2b063bd533b6e77f5a4ae3c512d25fd0
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-12 22:06:31 +0900

    feat: add RefreshResponse DTO and update SignUpRequest to include email

--- src/main/java/co/kr/bumaview/domain/auth/presentation/dto/req/SignUpRequest.java
+++ src/main/java/co/kr/bumaview/domain/auth/presentation/dto/req/SignUpRequest.java
@@ -1,6 +1,5 @@
 package co.kr.bumaview.domain.auth.presentation.dto.req;
 
-import co.kr.bumaview.domain.user.domain.type.Authority;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public class SignUpRequest {
     private String id;
+    private String email;
     private String nickname;
     private String password;
-    private Authority userType;
+    private String userType;
 }
