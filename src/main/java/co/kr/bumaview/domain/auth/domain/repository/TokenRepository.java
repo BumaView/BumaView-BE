4f203dd0209b43aa56b32c31a995394ca7c54942
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 04:22:45 +0900

    refactor: add Param annotation to findByUserId method in TokenRepository

--- src/main/java/co/kr/bumaview/domain/auth/domain/repository/TokenRepository.java
+++ src/main/java/co/kr/bumaview/domain/auth/domain/repository/TokenRepository.java
@@ -3,6 +3,7 @@ package co.kr.bumaview.domain.auth.domain.repository;
 import co.kr.bumaview.domain.auth.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,5 +12,5 @@ public interface TokenRepository extends JpaRepository<RefreshToken, Long> {
     int deleteByRefreshToken(String token);
 
     @Query("SELECT r FROM RefreshToken r WHERE r.id = :id")
-    Optional<RefreshToken> findByUserId(String id);
+    Optional<RefreshToken> findByUserId(@Param("id") String id);
 }
