d2d63fe1803195474458bf69c2602df82faa8f4e
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 06:19:02 +0900

    feat: integrate QuestionCommandService into QuestionAdminController for question creation

--- src/main/java/co/kr/bumaview/domain/question/presentation/QuestionAdminController.java
+++ src/main/java/co/kr/bumaview/domain/question/presentation/QuestionAdminController.java
@@ -1,7 +1,9 @@
 package co.kr.bumaview.domain.question.presentation;
 
+import co.kr.bumaview.domain.question.domain.service.command.QuestionCommandService;
 import co.kr.bumaview.domain.question.presentation.dto.req.QuestionRequest;
 import co.kr.bumaview.domain.question.presentation.dto.res.QuestionResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin/questions")
+@RequiredArgsConstructor
 public class QuestionAdminController {
+
+    private final QuestionCommandService questionCommandService;
 
     @PostMapping
     public ResponseEntity<QuestionResponse> handleQuestions(
             @RequestBody QuestionRequest requestDto
     ) {
         // 질문 처리 로직
+        requestDto.getQuestions().forEach(q ->
+                questionCommandService.createQuestion(q.getQ())
+        );
         return ResponseEntity.ok(new QuestionResponse("등록되었습니다"));
     }
 }
