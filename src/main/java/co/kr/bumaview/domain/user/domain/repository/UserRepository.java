191d881ded4b1a2a349b94b1d1cce737e35bec83
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 04:22:35 +0900

    refactor: update import for Param annotation to use Spring Data repository

--- src/main/java/co/kr/bumaview/domain/user/domain/repository/UserRepository.java
+++ src/main/java/co/kr/bumaview/domain/user/domain/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package co.kr.bumaview.domain.user.domain.repository;
 
 import co.kr.bumaview.domain.user.domain.User;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
