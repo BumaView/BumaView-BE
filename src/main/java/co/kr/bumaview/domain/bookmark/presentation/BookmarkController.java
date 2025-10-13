985f680017862570326b9af0f6b7d654cc3fe409
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 22:07:39 +0900

    feat: Implement BookmarkController for folder creation

---
+++ src/main/java/co/kr/bumaview/domain/bookmark/presentation/BookmarkController.java
@@ -0,0 +1,32 @@
+package co.kr.bumaview.domain.bookmark.presentation;
+
+import co.kr.bumaview.domain.bookmark.presentation.dto.req.CreateFolderRequest;
+import co.kr.bumaview.domain.bookmark.presentation.dto.res.FolderResponse;
+import co.kr.bumaview.domain.bookmark.service.BookmarkService;
+import co.kr.bumaview.domain.user.domain.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bookmarks")
+@PreAuthorize("hasRole('USER')")
+@RequiredArgsConstructor
+public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    @PostMapping("/folders")
+    public ResponseEntity<FolderResponse> createFolder(
+            @RequestBody CreateFolderRequest request,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        FolderResponse response = bookmarkService.createFolder(request, userDetails.getUserId());
+        return ResponseEntity.ok(response);
+    }
+}
