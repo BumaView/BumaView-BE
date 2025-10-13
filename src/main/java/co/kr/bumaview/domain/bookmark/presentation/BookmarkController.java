ac1b757f0d69ebf6ff76c15123526daf257c2d34
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 22:26:05 +0900

    feat: Add createBookmark endpoint to BookmarkController

--- src/main/java/co/kr/bumaview/domain/bookmark/presentation/BookmarkController.java
+++ src/main/java/co/kr/bumaview/domain/bookmark/presentation/BookmarkController.java
@@ -1,6 +1,8 @@
 package co.kr.bumaview.domain.bookmark.presentation;
 
+import co.kr.bumaview.domain.bookmark.presentation.dto.req.CreateBookmarkRequest;
 import co.kr.bumaview.domain.bookmark.presentation.dto.req.CreateFolderRequest;
+import co.kr.bumaview.domain.bookmark.presentation.dto.res.BookmarkResponse;
 import co.kr.bumaview.domain.bookmark.presentation.dto.res.FolderResponse;
 import co.kr.bumaview.domain.bookmark.service.BookmarkService;
 import co.kr.bumaview.domain.user.domain.CustomUserDetails;
@@ -27,6 +29,15 @@ public class BookmarkController {
             @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
         FolderResponse response = bookmarkService.createFolder(request, userDetails.getUserId());
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<BookmarkResponse> createBookmark(
+            @RequestBody CreateBookmarkRequest request,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        BookmarkResponse response = bookmarkService.createBookmark(request, userDetails.getUserId());
         return ResponseEntity.ok(response);
     }
 }
