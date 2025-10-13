9447ad1625f4f1792e63b690f1d0d0ef19b77868
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-14 00:18:24 +0900

    feat: Add question relationship to Bookmark entity and repository method

--- src/main/java/co/kr/bumaview/domain/bookmark/domain/Bookmark.java
+++ src/main/java/co/kr/bumaview/domain/bookmark/domain/Bookmark.java
@@ -1,5 +1,6 @@
 package co.kr.bumaview.domain.bookmark.domain;
 
+import co.kr.bumaview.domain.question.domain.Question;
 import jakarta.persistence.Table;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -14,9 +15,16 @@ public class Bookmark {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long questionId;
     private Long folderId;
     private String userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", insertable = false, updatable = false)
+    private Question question;
+
+    @Column(name = "question_id")
+    private Long questionId;
+
 
     public Bookmark(Long questionId, Long folderId, String userId) {
         this.questionId = questionId;
