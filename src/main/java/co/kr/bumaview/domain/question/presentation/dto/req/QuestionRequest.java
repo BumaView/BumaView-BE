9bffcd88b3caa3a3adec9c3fb1eb3830147580f0
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 10:27:59 +0900

    feat: add NotBlank validation to questions in QuestionRequest

--- src/main/java/co/kr/bumaview/domain/question/presentation/dto/req/QuestionRequest.java
+++ src/main/java/co/kr/bumaview/domain/question/presentation/dto/req/QuestionRequest.java
@@ -1,11 +1,13 @@
 package co.kr.bumaview.domain.question.presentation.dto.req;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import java.util.List;
 
 @Getter
 public class QuestionRequest {
 
+    @NotBlank(message = "질문은 비어있을 수 없습니다")
     private List<Question> questions;
 
     @Getter
