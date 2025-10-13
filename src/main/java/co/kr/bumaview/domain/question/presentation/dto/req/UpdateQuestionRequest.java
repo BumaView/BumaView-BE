5e4ce14ba2742afbb3de2f6d7a799c1640762aaa
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 16:51:21 +0900

    feat: add UpdateQuestionRequest and UpdateQuestionResponse classes

---
+++ src/main/java/co/kr/bumaview/domain/question/presentation/dto/req/UpdateQuestionRequest.java
@@ -0,0 +1,8 @@
+package co.kr.bumaview.domain.question.presentation.dto.req;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateQuestionRequest {
+    private String q;
+}
---
+++ src/main/java/co/kr/bumaview/domain/question/presentation/dto/res/UpdateQuestionResponse.java
@@ -0,0 +1,3 @@
+package co.kr.bumaview.domain.question.presentation.dto.res;
+
+public record UpdateQuestionResponse(Long id, String message) { }
