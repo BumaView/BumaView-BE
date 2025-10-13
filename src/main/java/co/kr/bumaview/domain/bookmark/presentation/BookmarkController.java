687e5df239d63d6cafced9457bd187d1fe188247
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 22:29:06 +0900

    refactor: 로그인 한 유저면 접근 가능하게 수정

--- src/main/java/co/kr/bumaview/domain/bookmark/presentation/BookmarkController.java
+++ src/main/java/co/kr/bumaview/domain/bookmark/presentation/BookmarkController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/bookmarks")
-@PreAuthorize("hasRole('USER')")
+@PreAuthorize("isAuthenticated()")
 @RequiredArgsConstructor
 public class BookmarkController {
 
