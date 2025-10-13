372f0688e915be692fef18d99290bf3821adf427
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 11:22:24 +0900

    fix: update validation for questions in QuestionRequest

--- src/main/java/co/kr/bumaview/domain/question/presentation/dto/req/QuestionRequest.java
+++ src/main/java/co/kr/bumaview/domain/question/presentation/dto/req/QuestionRequest.java
@@ -1,17 +1,21 @@
 package co.kr.bumaview.domain.question.presentation.dto.req;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import java.util.List;
 
 @Getter
 public class QuestionRequest {
 
-    @NotBlank(message = "질문은 비어있을 수 없습니다")
+    @NotEmpty(message = "질문 목록은 비어있을 수 없습니다")
+    @Valid
     private List<Question> questions;
 
     @Getter
     public static class Question {
+        @NotBlank(message = "질문 내용은 비어있을 수 없습니다")
         private String q;
     }
 }
