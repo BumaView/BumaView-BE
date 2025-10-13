430563d3fbf4f4662d27fa762b71c06a1866e800
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-14 06:02:36 +0900

    feat: Add endpoint to create mock interviews in InterviewController

--- src/main/java/co/kr/bumaview/domain/interview/presentation/InterviewController.java
+++ src/main/java/co/kr/bumaview/domain/interview/presentation/InterviewController.java
@@ -1,24 +1,32 @@
 package co.kr.bumaview.domain.interview.presentation;
 
+import co.kr.bumaview.domain.interview.presentation.dto.req.CreateInterviewReq;
+import co.kr.bumaview.domain.interview.presentation.dto.res.CreateInterviewRes;
+import co.kr.bumaview.domain.interview.service.InterviewService;
 import co.kr.bumaview.domain.question.domain.Question;
 import co.kr.bumaview.domain.question.domain.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("api/interviews")
+@RequestMapping("user/interviews")
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('USER')")
 public class InterviewController {
 
     private final QuestionRepository questionRepository;
+    private final InterviewService interviewService;
 
     @GetMapping("/random")
     public Question getRandomInterview() {
         return questionRepository.findRandomQuestion();
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateInterviewRes> createInterview(@RequestBody CreateInterviewReq req) {
+        return ResponseEntity.ok(interviewService.createInterview(req));
     }
 }
