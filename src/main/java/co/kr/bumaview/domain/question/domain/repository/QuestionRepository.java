df8a7cd0a6f831c8bd72a1661926963a0b22c77f
hxeonxins <hyunjin.dev143@gmail.com>
2025-10-13 18:59:20 +0900

    feat: add filtering functionality to QuestionController and repository

--- src/main/java/co/kr/bumaview/domain/question/domain/repository/QuestionRepository.java
+++ src/main/java/co/kr/bumaview/domain/question/domain/repository/QuestionRepository.java
@@ -1,7 +1,11 @@
 package co.kr.bumaview.domain.question.domain.repository;
 
 import co.kr.bumaview.domain.question.domain.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    Page<Question> findByQuestionContaining(String query, Pageable pageable);
 }
